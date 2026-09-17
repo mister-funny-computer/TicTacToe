b737a1e99869ac346e85b58014f815b7e3202fc3
Mister funny computer <tironasaurusnik@gmail.com>
2026-09-17 19:57:19 +0300

    Do beautiful

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 area = ["*", "*", "*"]
 
-print(area)
+#print(area)
 
 area[1] = "X"
 area[2] = "0"
 
 for cell in area:
-    print(cell, end=" ")
+    #print(cell, end=" ")
+    pass
 
 print()
 print()
+
+
 
 area = [
     ["*", "*", "*"],
@@ -17,21 +20,31 @@ area = [
     ["*", "*", "*"]
 ]
 
-area[2][2] = "X"
+def print_area():
+    print()
+    print("|-------|")
+    for row in area:
+        print("| ", end="")
+        for cell in row:
+            print(cell, end=" ")
+        print("|")
+    print("|-------|")
+    print()
 
-row = area[1]
-row[1] = "0"
+#area[2][2] = "X"
 
-print("|-------|")
-for row in area:
-    print("| ", end="")
-    for cell in row:
-        print(cell, end=" ")
-    print("|")
-print("|-------|")
+#row = area[1]
+#row[1] = "0"
 
+print_area()
 
-row = input("Введите индекс строки (1 строчка - 0, 2 строчка - 1, 3 строчка - 2): ")
-column = input("Введите индекс столбца (1 столбец - 0, 2 столбец - 1, 3 столбец - 2): ")
+row = int(input("Введите индекс строки (1 строчка - 0, 2 строчка - 1, 3 строчка - 2): "))
+column = int(input("Введите индекс столбца (1 столбец - 0, 2 столбец - 1, 3 столбец - 2): "))
 
 area[row][column] = "X"
+
+print_area()
+
+for i in range(1, 10):
+    print(i)
+
