69b35dac9290df2e4a3e6a0ea5e93ff1be312e62
Mister funny computer <tironasaurusnik@gmail.com>
2026-09-22 19:59:28 +0300

    Do 9 move

--- main.py
+++ main.py
@@ -38,13 +38,22 @@ def print_area():
 
 print_area()
 
-row = int(input("Введите индекс строки (1 строчка - 0, 2 строчка - 1, 3 строчка - 2): "))
-column = int(input("Введите индекс столбца (1 столбец - 0, 2 столбец - 1, 3 столбец - 2): "))
+move = "X"
 
-area[row][column] = "X"
-
-print_area()
 
 for i in range(1, 10):
-    print(i)
+    row = int(input("Введите индекс строки (1 строчка - 0, 2 строчка - 1, 3 строчка - 2): "))
+    column = int(input("Введите индекс столбца (1 столбец - 0, 2 столбец - 1, 3 столбец - 2): "))
+
+    if i % 2 == 0:
+        move = "0"
+    else:
+        move = "X"
+
+    area[row][column] = move
+
+    print_area()
+
+
+
 
